96e8914677001b5825b7099a4c1633ab1444265c
Graceey09 <134516715+Graceey09@users.noreply.github.com>
2023-10-10 23:46:11 +0100

    Update reverseArray.js
    code updated

--- Introduction/Arrays/reverseArray.js
+++ Introduction/Arrays/reverseArray.js
@@ -13,18 +13,15 @@ function reverseArrayInPlace(arr) {
     let left = 0;
     let right = arr.length - 1;
     while (left < right) {
-        // Swap elements at left and right indices
         let temp = arr[left];
         arr[left] = arr[right];
         arr[right] = temp;
-        // Move the indices towards the center
         left++;
         right--;
     }
     return arr;
 }
 
-// Example usage of reverseArrayInPlace function
 let arrayToReverse = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayToReverse);
 console.log(reverseMyArray([1, 2, 3, 4, 5]))
